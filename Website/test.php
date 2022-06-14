edc42f7a88924fc53eb7bb5ab25960a1d3327841
HHenke <holthenke1@gmail.com>
2022-06-14 10:54:39 -0400

    DB start

--- Website/test.php
+++ Website/test.php
@@ -1,6 +1,6 @@
 <?php
 //Step 2.1 - Remove this comment when done
-$con=mysqli_connect("db.luddy.indiana.edu","i308f20_hhenke","my+sql=i308f20_hhenke", "i308f20_hhenke");
+$con=mysqli_connect("--------------");
 // Check connection
 if (!$con)
 	{die("Failed to connect to MySQL: " . mysqli_connect_error() ); }
@@ -9,10 +9,18 @@ else
 	
 //Step 2.2 - Uncomment the following when instructed by removing the /* and the */ below.
 
-$var_cname = $_POST['form-cname'];
-$var_cadress = $_POST['form-caddress'];
-$var_cphone = $_POST['form-cphone'];
-$sql = "INSERT INTO customer (name, address, phone) VALUES ('$var_cname', '$var_caddress', '$var_cphone') ";
+$var_??? = $_POST['----'];
+$var_??? = $_POST['----'];
+$var_??? = $_POST['----'];
+$sql = "INSERT INTO Product (name, address, phone) VALUES ('$var_???', '$$var_???', '$$var_???') ";
+
+
+
+
+
+
+
+
 
 if (mysqli_query($con, $sql)) {
 	echo "1 record added";
