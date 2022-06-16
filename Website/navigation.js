aa3bd26882bb0be9c916c1dad6ca322c177fe230
Jared Armstrong <97423883+jared-armstrong@users.noreply.github.com>
2022-06-16 15:20:02 -0400

    new changes

---
+++ Website/navigation.js
@@ -0,0 +1,37 @@
+function navToHome() {
+    window.location.href = "C:/Users/armstrongja/OneDrive%20-%20Hat%20World%2C%20Inc/Documents/GitHub/IT-Interns-2022/Website/index.html";
+}
+
+function navToPricing() {
+    window.location.href = "C:/Users/armstrongja/OneDrive%20-%20Hat%20World%2C%20Inc/Documents/GitHub/IT-Interns-2022/Website/pricingHome.html";
+}
+
+function navToGenerate() {
+    window.location.href = "C:/Users/armstrongja/OneDrive%20-%20Hat%20World%2C%20Inc/Documents/GitHub/IT-Interns-2022/Website/homeSKU.html";
+}
+
+function navToFinder() {
+    window.location.href = "C:/Users/armstrongja/OneDrive%20-%20Hat%20World%2C%20Inc/Documents/GitHub/IT-Interns-2022/Website/findSKU.html";
+}
+
+function navToUPC() {
+    window.location.href = "C:/Users/armstrongja/OneDrive%20-%20Hat%20World%2C%20Inc/Documents/GitHub/IT-Interns-2022/Website/upcUpload.html";
+}
+
+//function navToVendor()
+
+function navToManual() {
+    window.location.href = "C:/Users/armstrongja/OneDrive%20-%20Hat%20World%2C%20Inc/Documents/GitHub/IT-Interns-2022/Website/singleSKU.html";
+}
+
+function navToMassSKU() {
+    window.location.href = "C:/Users/armstrongja/OneDrive%20-%20Hat%20World%2C%20Inc/Documents/GitHub/IT-Interns-2022/Website/massSKU.html";
+}
+
+function navToSinglePrice() {
+
+}
+
+function navToMassPrice() {
+    
+}
