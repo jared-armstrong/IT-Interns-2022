28a46c81381fef8a3cad2dbc8945ca0df8e6da85
HHenke <holthenke1@gmail.com>
2022-06-30 16:50:51 -0400

    mess of commit

--- Website/php/testReadCSV.php
+++ Website/php/testReadCSV.php
@@ -1,9 +1,9 @@
 <?php
 
-$myfile = fopen("php\\test.csv", "r") or die("Unable to open file!");
+$myfile = fopen("C:\Users\henkeh\Desktop\gest2.csv", "r") or die("Unable to open file!");
 
 // Get total number of rows
-$rowLength = file('test.csv');
+//$rowLength = file('test.csv');
 
 // Flag is used to skip first row in csv (header)
 $flag = true;
@@ -25,7 +25,6 @@ while (($line = fgetcsv($myfile, 1000, ",")) !== FALSE) {
         // Not actual functionality. Needs to append to array for SQL statement
         echo $line[$key];
         $key++;
-
         // Use to break lines. Only useful for testing in console
         if ($key == ($colLength)) {
             echo "\n";
